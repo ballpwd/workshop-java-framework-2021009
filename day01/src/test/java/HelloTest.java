a6872f9ffaf61493e7cc7f10a85bdfe74139600c
K0966211 <puwadech.s@kbtg.tech>
2021-09-06 10:07:56 +0700

    first commit

---
+++ day01/src/test/java/HelloTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class HelloTest {
+
+        @Test
+        public void case01(){
+                //Arrange
+                Hello hello = new Hello();
+                //Act
+                String actualResult = hello.sayHi("puwadech") ;
+                //Assert
+                assertEquals("Hello, puwadech", actualResult);
+        }
+
+}
